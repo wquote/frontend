64f88bc4d21357562f42cafae4253864fa53619f
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2023-02-06 23:44:00 +0000

    create home and customers components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { CustomerListComponent } from './views/customer/customer-list/customer-list.component';
+import { CustomerEditComponent } from './views/customer/customer-edit/customer-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    CustomerListComponent,
+    CustomerEditComponent
   ],
   imports: [
     BrowserModule,
