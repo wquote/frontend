f5f88784b62d216bef0bc08208e017209b074bbe
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2023-02-07 00:27:35 +0000

    component files organization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerEditComponent } from './views/customer-edit/customer-edit.component';
+import { CustomerListComponent } from './views/customer-list/customer-list.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+
+  { path: 'customers/list', component: CustomerListComponent },
+  { path: 'customers/edit/:id', component: CustomerEditComponent },
+  { path: 'customers/new', component: CustomerEditComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { HomeComponent } from './views/home/home.component';
-import { CustomerListComponent } from './views/customer/customer-list/customer-list.component';
-import { CustomerEditComponent } from './views/customer/customer-edit/customer-edit.component';
+import { CustomerListComponent } from './views/customer-list/customer-list.component';
+import { CustomerEditComponent } from './views/customer-edit/customer-edit.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    SideBarComponent,
     HomeComponent,
     CustomerListComponent,
     CustomerEditComponent
