23a04e0c0d2bbb60bec2c322956dcac32434db0c
Cleto May <cleto.may@hotmail.com>
2023-08-20 13:18:53 -0300

    Implementação de inclusao do job address.

--- src/app/views/deck-quote-edit/dqe-customer/dqe-customer.component.ts
+++ src/app/views/deck-quote-edit/dqe-customer/dqe-customer.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CustomerModel } from 'src/app/models/customer.model';
 import { DeckQuoteModel } from 'src/app/models/deck-quote.model';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-dqe-customer',
@@ -13,21 +14,62 @@ export class DqeCustomerComponent implements OnInit {
   deckQuote: DeckQuoteModel | undefined
 
   @Input()
-  customer: CustomerModel | undefined 
+  customer: CustomerModel | undefined
 
   newAddress: string | undefined
 
+  constructor(private customerService: CustomerService) { }
+
   ngOnInit() {
-    if(this.deckQuote && !this.deckQuote.jobAddress && this.customer && this.customer.homeAddress){
+    if (this.deckQuote && !this.deckQuote.jobAddress && this.customer && this.customer.homeAddress) {
       this.deckQuote.jobAddress = this.customer.homeAddress
     }
   }
 
-  changeJobAddress(address: string | undefined){
-    if(!this.deckQuote){
+  changeJobAddress(address: string | undefined) {
+    if (!this.deckQuote) {
       return
     }
     this.deckQuote.jobAddress = address
+  }
+
+  addNewAddress() {
+    if (!this.newAddress) {
+      return
+    }
+    if (!this.customer?.id) {
+      return
+    }
+    if (this.customer && !this.customer?.jobAddress) {
+      this.customer.jobAddress = []
+    }
+
+    if (this.customer.jobAddress.indexOf(this.newAddress) >= 0){
+      return
+    }
+
+    this.customer?.jobAddress.push(this.newAddress)
+    this.customerService.update(this.customer?.id, this.customer).subscribe(() => {
+      if (!this.deckQuote) {
+        return
+      }
+      this.deckQuote.jobAddress = this.newAddress
+      this.newAddress = undefined
+    })
+  }
+
+  removeJobAddress(address: string) {
+    if (!this.customer) {
+      return
+    }
+    if (!this.customer.id) {
+      return
+    }
+    this.customer.jobAddress = this.customer.jobAddress.filter(a => a != address);
+    if (this.deckQuote && this.deckQuote.jobAddress == address) {
+      this.deckQuote.jobAddress = undefined
+    }
+    this.customerService.update(this.customer.id, this.customer).subscribe(() => { })
   }
 
 }
