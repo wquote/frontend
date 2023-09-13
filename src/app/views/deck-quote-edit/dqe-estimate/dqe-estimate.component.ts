84baec5b154ad7971616178f7b73d2651ab170e1
Cleto May <cleto.may@hotmail.com>
2023-09-13 18:23:47 -0300

    Implementacao da matriz de custo.

--- src/app/views/deck-quote-edit/dqe-estimate/dqe-estimate.component.ts
+++ src/app/views/deck-quote-edit/dqe-estimate/dqe-estimate.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, defineInjectable } from '@angular/core';
 import { DeckQuoteModel } from 'src/app/models/deck-quote.model';
 
 @Component({
@@ -8,7 +8,34 @@ import { DeckQuoteModel } from 'src/app/models/deck-quote.model';
 })
 export class DqeEstimateComponent {
 
+  menu: string = 'material'
+
   @Input()
   deckQuote: DeckQuoteModel | undefined
+
+  minorCost(deckingIndex: number, railingIndex: number): boolean {
+
+    if (!this.deckQuote || !this.deckQuote.boardSpecs || !this.deckQuote.railingSpecs) {
+      return false
+    }
+
+    let minorDeckingIndex = 0
+    let minorRailingIndex = 0
+
+    this.deckQuote.boardSpecs.catalogsSpec.forEach((b, bi) => {
+      if(this.deckQuote && this.deckQuote.railingSpecs){
+        this.deckQuote.railingSpecs.catalogsSpec.forEach((r, ri) => {
+          if(this.deckQuote && this.deckQuote.boardSpecs && this.deckQuote.railingSpecs){
+            if(b.cost + r.cost < this.deckQuote.boardSpecs.catalogsSpec[minorDeckingIndex].cost + this.deckQuote.railingSpecs.catalogsSpec[minorRailingIndex].cost){
+              minorDeckingIndex = bi
+              minorRailingIndex = ri
+            }
+          }
+        })
+      }
+    })
+
+    return deckingIndex == minorDeckingIndex && railingIndex == minorRailingIndex
+  }
 
 }
