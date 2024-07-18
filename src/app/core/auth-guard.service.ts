94890e7bbe224b2eddcd3b522a4edc020bd8d2e4
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2024-07-18 14:05:11 +0000

    #31 feat: Add authGuard service

---
+++ src/app/core/auth-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from './auth.service'; // Adjust the path as necessary
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  // Define the canActivate function using CanActivateFn
+  canActivate: CanActivateFn = () => {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  };
+}
