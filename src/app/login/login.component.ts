1859df7d459c9eec8e0ae72a275b15a30021b959
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2024-07-18 14:03:40 +0000

    feat: Add login component #31

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../core/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  constructor(private authService: AuthService) { }
+
+  login(username: string, password: string) {
+    this.authService.login(username, password)
+  }
+}
