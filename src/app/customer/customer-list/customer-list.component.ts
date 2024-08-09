8acdd8341cff18b3420ea543c8c656240a8e9070
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2024-08-09 19:50:27 +0000

    #5 feat: search customer by name and address

--- src/app/customer/customer-list/customer-list.component.ts
+++ src/app/customer/customer-list/customer-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { CustomerModel } from 'src/app/customer/shared/customer.model';
-import { CustomerService } from '../shared/customer.service';
-
+import { Component, OnInit } from '@angular/core'
+import { CustomerModel } from 'src/app/customer/shared/customer.model'
+import { CustomerService } from '../shared/customer.service'
 
 
 @Component({
@@ -14,14 +13,36 @@ export class CustomerListComponent implements OnInit {
   customers: CustomerModel[] = []
   customerToDelete: CustomerModel | undefined
   deleteCustomerModal: any
+  searchTerm: string = ''
+  filteredCustomers: CustomerModel[] = []
 
   constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
-    this.customerService.readAll().subscribe(
-      result => {
-        this.customers = result
+    this.customerService.readAll()
+      .subscribe(customers => {
+        this.customers = customers
+        this.filteredCustomers = this.customers
       })
+  }
+
+  // Filter customers by first name, last name, or home address
+  filterCustomers(): void {
+    if (!this.searchTerm) {
+      this.filteredCustomers = this.customers
+      return
+    }
+
+    this.filteredCustomers = this.customers.filter(customer => {
+      const firstName: string | undefined = customer.firstName?.toLowerCase()
+      const lastName: string | undefined = customer.lastName?.toLowerCase()
+      const homeAddress: string | undefined = customer.homeAddress?.toLowerCase()
+      const searchTermLower = this.searchTerm.toLowerCase()
+
+      return firstName?.includes(searchTermLower) ||
+        lastName?.includes(searchTermLower) ||
+        homeAddress?.includes(searchTermLower)
+    })
   }
 
   updateCustomerToDelete(customer: CustomerModel) {
@@ -37,18 +58,18 @@ export class CustomerListComponent implements OnInit {
       }
     )
   }
-  
+
   formatPhones(phones: string[]): string {
     if (!phones) {
-      return '';
+      return ''
     }
-    
+
     return phones.map(phone => {
-      const match = phone.match(/^(\d{3})(\d{3})(\d{4})$/);
+      const match = phone.match(/^(\d{3})(\d{3})(\d{4})$/)
       if (match) {
-        return `(${match[1]}) ${match[2]}-${match[3]}`;
+        return `(${match[1]}) ${match[2]}-${match[3]}`
       }
-      return null;
-    }).join(', ');
+      return null
+    }).join(', ')
   }
 }
