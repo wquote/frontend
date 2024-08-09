27b0c529816ade2a0ec197cfc5db44f1ae18d398
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2024-08-09 19:51:09 +0000

    #35 feat: search quotes by customer name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,16 @@
 // - !trazer o nome do cliente e job address no quoteService readAll
 // - implementar a paginação 
 
-import { Component } from '@angular/core';
-import { CustomerModel } from 'src/app/customer/shared/customer.model';
-import { Quote } from "src/app/quotes/quote.model";
-import { CustomerService } from 'src/app/customer/shared/customer.service';
-import { QuoteService } from 'src/app/quotes/quote.service';
+import { Component } from '@angular/core'
+import { CustomerModel } from 'src/app/customer/shared/customer.model'
+import { Quote } from "src/app/quotes/quote.model"
+import { CustomerService } from 'src/app/customer/shared/customer.service'
+import { QuoteService } from 'src/app/quotes/quote.service'
+import { BehaviorSubject, Observable } from 'rxjs'
+
+class QuoteWithCustomerName extends Quote {
+  customerName?: string
+}
 
 @Component({
   selector: 'app-home',
@@ -15,14 +20,17 @@ import { QuoteService } from 'src/app/quotes/quote.service';
 })
 export class HomeComponent {
   quotes: Quote[] = []
-  quoteToDelete: Quote | undefined
-
+  quotesWithCustomerName: QuoteWithCustomerName[] = []
+  searchTerm: string = ''
+  filteredQuotes: QuoteWithCustomerName[] = []
+  quoteToDelete?: Quote
   customers: CustomerModel[] = []
+  customers$ = new BehaviorSubject<CustomerModel[]>([])
 
   constructor(
     private quoteService: QuoteService,
     private customerService: CustomerService,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.readAllCustomers()
@@ -30,31 +38,57 @@ export class HomeComponent {
   }
 
   readAllCustomers() {
-    this.customerService.readAll().subscribe( c => {
-      this.customers = c
-    })
+    this.customerService.readAll()
+      .subscribe(customers => {
+        this.customers = customers
+        this.customers$.next(customers)
+      })
   }
 
   readAllQuotes() {
-    this.quoteService.readAll().subscribe( q => {
-      q.sort( (a,b) => new Date(b.updatedAt || 0).getTime() - new Date(a.updatedAt || 0).getTime() )
-      this.quotes = q
-    })
+    this.quoteService.readAll()
+      .subscribe((quotes: Quote[]) => {
+        quotes.sort((a, b) => new Date(b.updatedAt || 0).getTime() - new Date(a.updatedAt || 0).getTime())
+        this.quotes = quotes
+
+        this.customers$.subscribe(_ => {
+          this.quotesWithCustomerName = quotes.map((quote: Quote) => {
+            return ({
+              ...quote,
+              customerName: this.getCustomerName(quote.customerId)
+            })
+          })
+          this.filteredQuotes = this.quotesWithCustomerName
+        })
+      })
   }
 
-  getCustomerName(id: string | undefined): string | undefined {
-    if(!id){
+
+  filterQuotes(): void {
+    if (!this.searchTerm) {
+      this.filteredQuotes = this.quotesWithCustomerName
       return
     }
 
-    let customer = this.customers.filter( c => c.id == id)[0]
+    this.filteredQuotes = this.quotesWithCustomerName.filter(quote => {
+      const customerName: string | undefined = this.getCustomerName(quote.customerId)
+      // const jobAddress: string | undefined = quote.jobAddress?.toLowerCase()
+      const searchTermLower = this.searchTerm.toLowerCase()
 
-    if(!customer){
-      return undefined
-    }
+      return customerName?.toLowerCase().includes(searchTermLower)
+      // || jobAddress?.includes(searchTermLower)
+    })
+  }
 
-    let name: string | undefined = customer.firstName + ' ' + customer.lastName
-    return name ? name : ''
+  getCustomerName(id?: string): string {
+    console.log('id', id)
+    if (!id) { return 'Unkown' }
+
+    let customer: CustomerModel = this.customers.filter(c => c.id == id)[0]
+    if (!customer) { return 'Unknown' }
+
+    let name: string = customer.lastName + ', ' + customer.firstName
+    return name
   }
 
   updateQuoteToDelete(item: Quote) {
