eeaa90aded528a167b34c15474bbd8356a180d1c
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2024-08-19 10:38:03 +0000

    #40 feat: adjustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
 import { CustomerListComponent } from './customer/customer-list/customer-list.component';
 import { CustomerQuotesListComponent } from './customer/customer-quotes-list/customer-quotes-list.component';
 import { DeckingQuoteEditComponent } from './decking-quotes/decking-quote-edit/decking-quote-edit.component';
+import { HomeComponent } from './home/home.component';
+import { MaterialListComponent } from './materials/material-list/material-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'landing', component: HomeComponent, pathMatch: 'full' },
-  
+
   { path: 'customers', component: CustomerListComponent },
   { path: 'customers/new', component: CustomerEditComponent },
   { path: 'customers/:id', component: CustomerEditComponent },
@@ -18,6 +19,9 @@ const routes: Routes = [
   // { path: 'customers/:customerId/quotes/new', component: DeckingQuoteEditComponent },
   { path: 'quotes/decking/new', component: DeckingQuoteEditComponent },
   { path: 'quotes/decking/:quoteId', component: DeckingQuoteEditComponent },
+
+  { path: 'materials', component: MaterialListComponent },
+  // { path: 'decking-material-order/footings', component: }
 ];
 
 @NgModule({
