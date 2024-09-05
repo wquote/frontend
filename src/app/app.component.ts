a88c582359187aef1317a3e34c5cb128b968ed23
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2024-09-05 17:50:13 +0000

    feat: only LoginComponent shown in /login url
    #43

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'quote-web';
+export class AppComponent implements OnInit {
+  title = 'wQuote'
+
+  constructor(
+    private authService: AuthService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    console.log('isAuthenticated', this.authService.isAuthenticated())
+  }
+
+  shouldBeShown() {
+    return this.authService.isAuthenticated()
+  }
 }
