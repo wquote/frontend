b4592d5a0844ba13a7665d5b0ec670a2e9ce276b
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2024-12-10 23:06:11 +0000

    feat: call api when at login page
    #42

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth.service';
+import { ApiService } from '../core/api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  constructor(private authService: AuthService) { }
+export class LoginComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private apiService: ApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.apiService.get("/").subscribe()
+  }
 
   login(username: string, password: string) {
     this.authService.login(username, password)
